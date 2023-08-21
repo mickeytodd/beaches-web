32ee507f8af945ef6141458264fc1d20806f7476
milanalakovic <milanalakovic1994@gmail.com>
2023-08-21 14:49:24 +0200

    fixed background img

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,23 +5,41 @@ import { useState } from 'react';
 const SearchBar = ({ data }) => {
     const [searchTerm, setSearchTerm] = useState('');
     const [filteredBeaches, setFilteredBeaches] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    const handleSearchChange = (event) => {
+    const handleInputChange = (event) => {
+        const newInputValue = event.target.value.trim();
         const newSearchTerm = event.target.value;
         setSearchTerm(newSearchTerm);
-
-
         const filtered = data.filter(beach =>
             beach.title.toLowerCase().includes(newSearchTerm.toLowerCase())
         );
         setFilteredBeaches(filtered);
+        setInputValue(newInputValue);
+        setIsDropdownOpen(newInputValue !== '');
     };
+
+    const handleInputBlur = () => {
+        setIsDropdownOpen(false);
+    };
+
+    const handleOutsideClick = (event) => {
+        if (event.target.id !== 'searchInput') {
+            setIsDropdownOpen(false);
+        }
+    };
+
+    React.useEffect(() => {
+        document.addEventListener('click', handleOutsideClick);
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, []);
 
     const handleBeachSelect = (beach) => {
-
         console.log('Selected beach:', beach);
     };
-
 
 
 
@@ -37,23 +55,29 @@ const SearchBar = ({ data }) => {
                     <input
                         type="text"
                         placeholder="Find a beach..."
-                        onChange={handleSearchChange}
                         className="text-input"
+                        id='searchInput'
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        onBlur={handleInputBlur}
                     />
                     <button className="search-button">Search</button>
                 </div>
-                <div>
-                    <ul className='search-results'>
-                        {filteredBeaches.map(beach => (
-                            <li
-                                key={beach.id}
-                                onClick={() => handleBeachSelect(beach)}
-                            >
-                                {beach.title}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+
+                {isDropdownOpen && (
+                    <div className='dropdown-container' id='dropdown-container'>
+                        <ul className='search-results'>
+                            {filteredBeaches.map(beach => (
+                                <li
+                                    key={beach.id}
+                                    onClick={() => handleBeachSelect(beach)}
+                                >
+                                    {beach.title}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
             </div>
             <p>Lorem ipsum dolor sit amet</p>
 
