80f383065623ed8397b06b9be344595c2ec8c162
milanalakovic <milanalakovic1994@gmail.com>
2023-11-15 14:57:00 +0100

    added slider buttons

--- src/components/BeachDetails/BeachSlider.js
+++ src/components/BeachDetails/BeachSlider.js
@@ -1,11 +1,12 @@
 // ImageSlider.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Slider from 'react-slick';
 import './BeachSlider.css';
 
 const BeachSlider = () => {
     const [images, setImages] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
         const fetchImages = async () => {
@@ -17,34 +18,50 @@ const BeachSlider = () => {
                 setImages(response.data.results);
             } catch (error) {
                 console.error('Error fetching images from Unsplash:', error);
+            } finally {
+                setLoading(false);
             }
         };
 
         fetchImages();
     }, []);
 
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+
+    const handlePrevClick = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    };
+
+    const handleNextClick = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
     };
 
 
     return (
-        <div className="beach-slider-container">
-            <div className="image-row">
-                {Array.isArray(images) &&
-                    images.map((image) => (
-                        <div key={image.id} className="image-item">
-                            <img src={image.urls.regular} alt={image.alt_description} />
+        <div className="image-slider-container">
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <div>
+                    <div className="top-image">
+                        {images.length > 0 && (
+                            <img src={images[currentIndex].urls.regular} alt={images[0].alt_description || 'Image'} />
+                        )}
+                        <div className="nav-buttons">
+                            <button onClick={handlePrevClick}>Previous</button>
+                            <button onClick={handleNextClick}>Next</button>
                         </div>
-                    ))}
-            </div>
+                    </div>
+                    <div className="image-row">
+                        {images.slice(1, 4).map((image, index) => (
+                            <div key={index} className="image-item">
+                                <img src={image.urls.regular} alt={image.alt_description || 'Image'} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
-    )
-        ;
-}
+    );
+};
 
 export default BeachSlider;
