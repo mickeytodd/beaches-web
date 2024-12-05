7a3e8afa94563c07d8675aecaf03d116b5a924d0
milanalakovic <milanalakovic1994@gmail.com>
2024-12-05 19:02:17 +0100

    changed header design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
             <div className='navbar__links'>
                 <ul className="navbar__menu">
                     <li><a href="#home">Home</a></li>
-                    <li><a href="#beaches">Beaches</a></li>
+                    <li><a href="#favorites">Favorites</a></li>
                     <li><a href="#activities">Activities</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
