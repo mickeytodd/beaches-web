9a9436ba929c58aedec631a6db53b96e411301c4
milanalakovic <milanalakovic1994@gmail.com>
2025-04-24 17:30:25 +0200

    logo font change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import Login from './Login/Login';
 
 const Navbar = () => {
     const [isLoginOpen, setIsLoginOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
         <div className='navbar__container'>
@@ -14,13 +15,13 @@ const Navbar = () => {
                     <a href="/">BeachFinder</a>
                 </div>
                 <div className='navbar__links'>
-                    <ul className="navbar__menu">
+                    <ul className={`navbar__menu ${isMenuOpen ? 'open' : ''}`}>
                         <li><a href="/">Home</a></li>
                         <li><a href="/profile">My Profile</a></li>
                         <li><a href="#favorites">Favorites</a></li>
                         <li><button className="login-btn" onClick={() => setIsLoginOpen(true)}>Log in</button></li>
                     </ul>
-                    <div className="navbar__hamburger">
+                    <div className="navbar__hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
                         <span></span>
                         <span></span>
                         <span></span>
