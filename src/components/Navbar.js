9feb9874a7553c82dc14b979d6e8850564d4a498
milanalakovic <milanalakovic1994@gmail.com>
2025-04-25 12:36:43 +0200

    styled dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import './Navbar.css';
 import UserAvatar from './UserAvatar';
 import userIcon from '../assets/images/user.png';
 import Login from './Login/Login';
+import { FaHome, FaUser, FaHeart } from 'react-icons/fa';
 
 const Navbar = () => {
     const [isLoginOpen, setIsLoginOpen] = useState(false);
@@ -15,9 +16,16 @@ const Navbar = () => {
         setIsDropdownOpen(!isDropdownOpen);
     };
 
+    const fakeUser = {
+        name: "Fake User",
+        imgUrl: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    };
+
+    const user = fakeUser;
+
     return (
         <div className='navbar__container'>
-            <nav className='navbar'>
+            <nav className="navbar">
                 <div className="navbar__logo">
                     <a href="/">BeachFinder</a>
                 </div>
@@ -41,13 +49,24 @@ const Navbar = () => {
                 </div>
             </nav>
             {isDropdownOpen && (
-                <ul className="navbar__dropdown--mobile">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/profile">My Profile</a></li>
-                    <li><a href="#favorites">Favorites</a></li>
-                </ul>
+                <div>
+                    <div className="mobile__menu">
+                        <button className="mobile__close" onClick={toggleDropdown}>✕</button>
+                        {user && (
+                            <div className="mobile__user">
+                                <img src={user.imgUrl} alt="User" className="mobile__user-img" />
+                                <div className="mobile__user-name">{user.name}</div>
+                            </div>
+                        )}
+                        <ul className="mobile__list">
+                            <li><a href="/"><FaHome className="nav-icon" />Home</a></li>
+                            <li><a href="/profile"><FaUser className="nav-icon" />My Profile</a></li>
+                            <li><a href="#favorites"><FaHeart className="nav-icon" />Favorites</a></li>
+                        </ul>
+                    </div>
+                    <div className="mobile__backdrop" onClick={toggleDropdown}></div>
+                </div>
             )}
-
             <Login isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
         </div>
     );
